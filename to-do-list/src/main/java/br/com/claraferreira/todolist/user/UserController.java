ef7893d1d30a0e61b42db69729369d1bc3d60de9
Clara <ferreirabatistaclara@gmail.com>
2023-10-13 17:49:23 -0300

    add_lombok

--- to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserController.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-
-
+        System.out.println(userModel.getUsername());
 
     }
 }
--- to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserModel.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserModel.java
@@ -1,7 +1,13 @@
 package br.com.claraferreira.todolist.user;
 
+import lombok.Data;
+
+@Data
+
 public class UserModel {
     private String username;
     private String name;
     private String password;
+
+
 }
