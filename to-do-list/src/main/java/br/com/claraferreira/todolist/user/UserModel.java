e03d252eeac8068ce6dcb1381b0542fca3c118a3
Clara <ferreirabatistaclara@gmail.com>
2023-10-14 19:45:01 -0300

    implemeted_h2_and_uuid

--- to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserModel.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserModel.java
@@ -1,13 +1,30 @@
 package br.com.claraferreira.todolist.user;
 
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+
+    private UUID id;
+
     private String username;
     private String name;
     private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
