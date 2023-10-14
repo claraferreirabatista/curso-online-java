330f3d83e343128da18a3d08449c624613bf917a
Clara <ferreirabatistaclara@gmail.com>
2023-10-14 20:47:29 -0300

    error_treatments_and_encryption

--- to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserController.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.claraferreira.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-
 public class UserController {
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
 
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
