a69907c56fd4c4df12611bb8ec55b2c4c37e0d8e
Clara <ferreirabatistaclara@gmail.com>
2023-10-15 11:27:00 -0300

    error_trataments

--- to-do-list/src/main/java/br/com/claraferreira/todolist/task/TaskControlller.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/task/TaskControlller.java
@@ -68,9 +68,22 @@ public class TaskControlller {
 
     // Definição do endpoint PUT para atualizar uma tarefa com base no seu ID
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var task = this.taskRepository.findById(id).orElse(null);
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Essa tarefa tem ID incorreto");
+        }
+
+        var idUser = request.getAttribute("idUser");
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Você não é autorizado para isso");
+        }
         Utils.copyNonNullProperties(taskModel, task);
-        return this.taskRepository.save(task);
+
+        var taskUpdated = this.taskRepository.save(task);
+
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
--- to-do-list/src/main/java/br/com/claraferreira/todolist/task/TaskModel.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/task/TaskModel.java
@@ -11,26 +11,35 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data  // Usando o Lombok para gerar automaticamente getters, setters, toString, etc.
+@Data // Usando o Lombok para gerar automaticamente getters, setters, toString, etc.
 
-@Entity(name = "tb_tasks")  // Esta classe é uma entidade JPA mapeada para a tabela "tb_tasks" no banco de dados.
+@Entity(name = "tb_tasks") // Esta classe é uma entidade JPA mapeada para a tabela "tb_tasks" no banco de
+                           // dados.
 
 public class TaskModel {
-    @Id  // A seguir, declaração do campo ID como a chave primária.
-    @GeneratedValue(generator = "UUID")  // O valor do campo ID será gerado automaticamente, usando UUID.
+    @Id // A seguir, declaração do campo ID como a chave primária.
+    @GeneratedValue(generator = "UUID") // O valor do campo ID será gerado automaticamente, usando UUID.
 
-    private UUID id;  // Campo de identificação exclusiva para cada tarefa.
-    private String name;  // Nome da tarefa.
-    private String description;  // Descrição da tarefa.
+    private UUID id; // Campo de identificação exclusiva para cada tarefa.
+    private String name; // Nome da tarefa.
+    private String description; // Descrição da tarefa.
 
-    @Column(length = 50)  // Especifica o comprimento máximo da coluna 'title' no banco de dados.
-    private String title;  // Título da tarefa.
-    private LocalDateTime startAt;  // Data e hora de início da tarefa.
-    private LocalDateTime endAt;  // Data e hora de término da tarefa.
-    private String priority;  // Prioridade da tarefa.
+    @Column(length = 50) // Especifica o comprimento máximo da coluna 'title' no banco de dados.
+    private String title; // Título da tarefa.
+    private LocalDateTime startAt; // Data e hora de início da tarefa.
+    private LocalDateTime endAt; // Data e hora de término da tarefa.
+    private String priority; // Prioridade da tarefa.
 
-    private UUID idUser;  // ID do usuário associado a esta tarefa.
+    private UUID idUser; // ID do usuário associado a esta tarefa.
 
-    @CreationTimestamp  // Esta anotação indica que o campo 'createdAt' será preenchido automaticamente com a data e hora de criação.
-    private LocalDateTime createdAt;  // Data e hora em que a tarefa foi criada.
+    @CreationTimestamp // Esta anotação indica que o campo 'createdAt' será preenchido automaticamente
+                       // com a data e hora de criação.
+    private LocalDateTime createdAt; // Data e hora em que a tarefa foi criada.
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo utrapassou quantidade máxima de caracteres");
+        }
+        this.title = title;
+    }
 }
