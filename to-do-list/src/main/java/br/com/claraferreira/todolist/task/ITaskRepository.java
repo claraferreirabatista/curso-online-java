773d2ceb0eda22716383d1a75485e0722206dd0b
Clara <ferreirabatistaclara@gmail.com>
2023-10-15 09:35:22 -0300

    create_task_list

--- to-do-list/src/main/java/br/com/claraferreira/todolist/task/ITaskRepository.java
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/task/ITaskRepository.java
@@ -3,7 +3,9 @@ package br.com.claraferreira.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    TaskModel findByTitle(String title);
+    List<TaskModel> findByIdUser(UUID idUser);
 }
