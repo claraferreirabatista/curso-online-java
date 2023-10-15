1a008622f49071895380bcd6e70b2f375d21b977
Clara <ferreirabatistaclara@gmail.com>
2023-10-14 22:07:48 -0300

    post_Task

---
+++ to-do-list/src/main/java/br/com/claraferreira/todolist/task/TaskControlller.java
@@ -0,0 +1,33 @@
+package br.com.claraferreira.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+
+public class TaskControlller {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+
+        var task = this.taskRepository.findByTitle(taskModel.getTitle());
+        if (task != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa existe");
+        }
+
+        var taskCreated = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+
+}
